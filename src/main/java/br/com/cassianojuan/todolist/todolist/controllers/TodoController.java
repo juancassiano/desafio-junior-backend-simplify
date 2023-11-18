cd7ba623c4abb93c7ba4787cf52e7977a9275c71
juancassiano <juancassiano@hotmail.com>
2023-11-18 05:36:35 -0300

    Criando estruturas de domínio da aplicação

---
+++ src/main/java/br/com/cassianojuan/todolist/todolist/controllers/TodoController.java
@@ -0,0 +1,32 @@
+package br.com.cassianojuan.todolist.todolist.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cassianojuan.todolist.todolist.entity.Todo;
+
+@RestController
+public class TodoController {
+  
+
+  List<Todo> create(Todo todo){
+
+  }
+
+  List<Todo> list(){
+    
+  }
+
+  List<Todo> update(Todo todo){
+    
+  }
+
+  List<Todo> delete(Long id){
+    
+  }
+
+  List<Todo> create(Todo todo){
+    
+  }
+}
