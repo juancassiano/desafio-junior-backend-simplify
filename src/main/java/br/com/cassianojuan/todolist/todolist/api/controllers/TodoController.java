c174bd0cb695462c8a2f471f736d59cb0ea06901
juancassiano <juancassiano@hotmail.com>
2023-11-18 09:13:19 -0300

    Criando DTOS para proteger entidade do BD

--- src/main/java/br/com/cassianojuan/todolist/todolist/api/controllers/TodoController.java
+++ src/main/java/br/com/cassianojuan/todolist/todolist/api/controllers/TodoController.java
@@ -3,7 +3,6 @@ package br.com.cassianojuan.todolist.todolist.api.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
