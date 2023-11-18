00c7d03c2760977bbd35cd67514b3e533c090cf9
juancassiano <juancassiano@hotmail.com>
2023-11-18 05:50:53 -0300

    Configurando controlador da API

--- src/main/java/br/com/cassianojuan/todolist/todolist/controllers/TodoController.java
+++ src/main/java/br/com/cassianojuan/todolist/todolist/controllers/TodoController.java
@@ -2,31 +2,45 @@ package br.com.cassianojuan.todolist.todolist.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cassianojuan.todolist.todolist.entity.Todo;
+import br.com.cassianojuan.todolist.todolist.service.TodoService;
 
-@RestController
+@RestController()
+@RequestMapping("/todos")
 public class TodoController {
   
+  private TodoService todoService;
 
-  List<Todo> create(Todo todo){
-
+  public TodoController(TodoService todoService) {
+    this.todoService = todoService;
   }
 
+  @PostMapping
+  List<Todo> create(@RequestBody Todo todo){
+    return todoService.create(todo);
+  }
+
+  @GetMapping
   List<Todo> list(){
-    
+    return todoService.list();
   }
 
-  List<Todo> update(Todo todo){
-    
+  @PutMapping()
+  List<Todo> update(@RequestBody Todo todo){
+    return todoService.update(todo);
   }
 
-  List<Todo> delete(Long id){
-    
-  }
-
-  List<Todo> create(Todo todo){
-    
+  @DeleteMapping("/{id}")
+  List<Todo> delete(@PathVariable Long id){
+    return todoService.delete(id);
   }
 }
